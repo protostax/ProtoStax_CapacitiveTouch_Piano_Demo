9cc8e341bbbe4d0a8d2cdf885a7ab862f43bc0da
sridhar.rajagopal@gmail.com <sridhar@upbeatlabs.com>
2019-08-15 10:35:50 -0700

    Multi-octave portable capacitive touch piano

--- ProtoStax_CapacitiveTouch_Piano_Demo.ino
+++ ProtoStax_CapacitiveTouch_Piano_Demo.ino
@@ -28,6 +28,7 @@
 
 
 #include <Wire.h>
+#include <Button.h>
 #include "Adafruit_MPR121.h"
 #include "pitches.h"
 #include "noteDurations.h"
@@ -35,6 +36,22 @@
 #ifndef _BV
 #define _BV(bit) (1 << (bit)) 
 #endif
+
+
+#define  SW1     2         //Connect a tactile button switch (or something similar)
+                           //from Arduino pin 2 to ground.
+#define  SW2     3         //Connect a tactile button switch (or something similar)
+                           //from Arduino pin 3 to ground.
+
+#define PULLUP true        //To keep things simple, we use the Arduino's internal pullup resistor.
+#define INVERT true        //Since the pullup resistor will keep the pin high unless the
+                           //switch is closed, this is negative logic, i.e. a high state
+                           //means the button is NOT pressed. (Assuming a normally open switch.)
+#define DEBOUNCE_MS 20     //A debounce time of 20 milliseconds usually works well for tactile button switches.
+#define LONG_PRESS 1000    //We define a "long press" to be 1000 milliseconds.
+
+Button upBtn(SW1, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
+Button downBtn(SW2, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
 
 // pitches.h defines all the frequencies for the different notes in different octaves
 // Let us pick one octave for our keyboard below, so it is easy to change it in one place
@@ -56,9 +73,17 @@
 // These correspond to the 12 physical capacitive touch keys of the Piano
 // Feel free to change the mapping to your own! 
 // notes in the scale:
-int scale[] = {
-  NOTE_C, NOTE_CS, NOTE_D, NOTE_DS, NOTE_E, NOTE_F, NOTE_FS, NOTE_G, NOTE_GS, NOTE_A, NOTE_AS, NOTE_B
+int scale[7][12] = {
+  {  NOTE_C1, NOTE_CS1, NOTE_D1, NOTE_DS1, NOTE_E1, NOTE_F1, NOTE_FS1, NOTE_G1, NOTE_GS1, NOTE_A1, NOTE_AS1, NOTE_B1 },
+  {  NOTE_C2, NOTE_CS2, NOTE_D2, NOTE_DS2, NOTE_E2, NOTE_F2, NOTE_FS2, NOTE_G2, NOTE_GS2, NOTE_A2, NOTE_AS2, NOTE_B2 },
+  {  NOTE_C3, NOTE_CS3, NOTE_D3, NOTE_DS3, NOTE_E3, NOTE_F3, NOTE_FS3, NOTE_G3, NOTE_GS3, NOTE_A3, NOTE_AS3, NOTE_B3 },
+  {  NOTE_C4, NOTE_CS4, NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4, NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4 },
+  {  NOTE_C5, NOTE_CS5, NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5 },
+  {  NOTE_C6, NOTE_CS6, NOTE_D6, NOTE_DS6, NOTE_E6, NOTE_F6, NOTE_FS6, NOTE_G6, NOTE_GS6, NOTE_A6, NOTE_AS6, NOTE_B6 },
+  {  NOTE_C7, NOTE_CS7, NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7 },
 };
+
+int octaveNum = 4;
 
 // The Piezo uses pin 12 in our example. Change as needed. Remember that you cannot use pins 3 and 11 
 // when using the tone library
@@ -129,19 +154,32 @@ void setup() {
     delay(10);
   }
   
-  Serial.println("ProtoStax Piano Demo"); 
+  Serial.println(F("ProtoStax Piano Demo")); 
   
   // Default address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!cap.begin(0x5A)) {
-    Serial.println("MPR121 not found, check wiring?");
+    Serial.println(F("MPR121 not found, check wiring?"));
     while (1);
   }
-  Serial.println("MPR121 found!");
+  Serial.println(F("MPR121 found!"));
   playTune(melody2, 23, 48);
 }
 
 void loop() {
+  upBtn.read();
+  downBtn.read();
+  static int lastOctave = 4;
+
+  lastOctave = octaveNum;
+  if (upBtn.isPressed()) {
+    octaveNum = 5;
+  } else if (downBtn.isPressed()) {
+    octaveNum = 3; 
+  } else {
+    octaveNum = 4;
+  }
+    
   // Get the currently touched pads
   currtouched = cap.touched();
 
@@ -155,12 +193,12 @@ void loop() {
   } else if (currtouched == 3075) { // C C# A# B
     playTune(melody4, MELODY_LENGTH(melody4), 1);  // Play Frere Jacques
   } else {
-  
+
     for (uint8_t i=0; i<12; i++) {
       // if it *is* touched and *wasnt* touched before, play the corresponding tone!
-      if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      if ((currtouched & _BV(i)) && (!(lasttouched & _BV(i)) || (lastOctave != octaveNum) )) {
         // Serial.print("currtouched = "); Serial.print(currtouched); Serial.print(" lasttouched = "); Serial.print(lasttouched); Serial.print(" "); Serial.print(i); Serial.println(" touched");
-        tone(TONE_PIN, scale[i]);
+        tone(TONE_PIN, scale[octaveNum][i]);
         
       }
       // if it *was* touched and now *isnt*, and additionally, no button is being pressed, then stop the tone
